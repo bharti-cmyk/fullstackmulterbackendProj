e1b5b41eb43bd34af91dfc09cae1d92d0ec1cb6e
Bharti <bhartimishra1993@gmail.com>
2024-03-28 16:02:19 +0530

    new1 commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const db = require('./db');
 const router = require('./routes/router');
 const app = new express();
-const port = 8000;
+require("dotenv").config();
 const cors = require('cors');
 // app.use(bodyParser.urlencoded({ extended: true }));
-
+const port = process.env.PORT || 8000;
 app.use(router);
 app.use(express.json());
 app.use(cors());
